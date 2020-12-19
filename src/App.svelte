e8fb1dbae8c01ee3e8637ac9088b926a0f059434
Anh Nguyen <nmqanh@gmail.com>
2020-12-19 22:10:39 +1000

    Handle dynamic tooltip based on fitting content; Add Examples usecases

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,83 @@
 <script>
   import SvelteTooltipify from './SvelteTooltipify.svelte'
-  export let name
 </script>
 
 <style type="text/scss">
-  $color: #ff3e00;
-
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  a {
+    cursor: pointer;
   }
 
-  h1 {
-    color: $color;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .fixed {
+    position: fixed;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .top {
+    top: 0;
+  }
+
+  .bottom {
+    bottom: 0;
+  }
+
+  .left {
+    left: 0;
+  }
+
+  .right {
+    right: 0;
+  }
+
+  .center {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .middle {
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .middleCenter {
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
   }
 </style>
 
 <main>
-  <SvelteTooltipify content="Hello">
-    <h1>Hello {name}!</h1>
-  </SvelteTooltipify>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <div class="fixed top left">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed top center">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed top right">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed middle left">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed middleCenter">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed middle right">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed bottom left">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed bottom center">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
+
+  <div class="fixed bottom right">
+    <SvelteTooltipify content="This is a tooltip"><a>Open tooltip</a></SvelteTooltipify>
+  </div>
 </main>
--- src/SvelteTooltipify.svelte
+++ src/SvelteTooltipify.svelte
@@ -1,6 +1,67 @@
 <script>
-  const horizontalPosition = 'left'
-  const verticalPosition = 'top'
+  import { afterUpdate } from 'svelte'
+  let horizontalPosition = 'center'
+  let verticalPosition = 'top'
+  let active = false
+  let fittingChecked = false
+  let tooltipContentNode = null
+  let wrapperNode = null
+
+  const handleOnMouseOver = ({ currentTarget }) => {
+    active = true
+  }
+  const handleOnMouseLeave = (e) => {
+    horizontalPosition = 'center'
+    verticalPosition = 'top'
+    active = false
+    fittingChecked = false
+  }
+
+  afterUpdate(() => {
+    if (active && !fittingChecked) {
+      if (!tooltipContentNode || !wrapperNode) {
+        return
+      }
+
+      const documentWidth = document.documentElement.clientWidth
+      const documentHeight = document.documentElement.clientHeight
+
+      let wrapperRect = wrapperNode.getBoundingClientRect()
+      let rightPos = wrapperRect.x + wrapperRect.width
+      let leftPos = wrapperRect.x
+      let topPos = wrapperRect.y
+      let bottomPos = wrapperRect.y + wrapperRect.height
+
+      let tooltipContentRect = tooltipContentNode.getBoundingClientRect()
+
+      let fitRight = rightPos + tooltipContentRect.width <= window.scrollX + documentWidth
+      let fitLeft = leftPos - tooltipContentRect.width >= 0
+
+      let fitTop = topPos - tooltipContentRect.height >= 0
+      let fitBottom = bottomPos + tooltipContentRect.height <= window.scrollY + documentHeight
+
+      if (fitTop) {
+        verticalPosition = 'top'
+      } else if (fitBottom) {
+        verticalPosition = 'bottom'
+      } else {
+        verticalPosition = 'bottom'
+      }
+
+      if (fitLeft && fitRight) {
+        horizontalPosition = 'center'
+      } else if (fitLeft) {
+        horizontalPosition = 'left'
+      } else if (fitRight) {
+        horizontalPosition = 'right'
+      } else {
+        horizontalPosition = 'center'
+      }
+
+      fittingChecked = true
+    }
+  })
+
   export let content = ''
   export let backgroundColor = '#000000'
   export let textColor = '#ffffffff'
@@ -8,13 +69,16 @@
 
 <style type="text/scss">
   $arrowSize: 10px;
-  $arrowPadding: 5px;
+  $arrowPadding: 3px;
   $arrowPosition: $arrowSize + $arrowPadding;
 
   .tooltip {
     position: relative;
+    display: inline-block;
+    box-sizing: border-box;
 
     .tooltip-arrow {
+      display: none;
       position: absolute;
       width: 0;
       height: 0;
@@ -23,38 +87,43 @@
       border-style: solid;
 
       &.bottom {
-        bottom: 0;
         left: 50%;
+        top: 100%;
         margin-left: -$arrowSize;
         border-width: 0 $arrowSize $arrowSize;
         border-bottom-color: var(--background-color);
       }
 
       &.top {
-        top: 0%;
-        bottom: -$arrowSize;
+        top: -$arrowSize;
         left: 50%;
         margin-left: -$arrowSize;
         border-width: $arrowSize $arrowSize 0;
         border-top-color: var(--background-color);
       }
+
+      &.active {
+        display: block;
+      }
     }
 
     .tooltip-content {
+      display: none;
       padding: 3px 8px;
       color: #fff;
       position: absolute;
       text-align: center;
       background-color: var(--background-color);
       border-radius: 4px;
-      max-width: 200px;
+      width: auto;
+      white-space: nowrap;
 
       &.bottom {
-        top: 100%;
+        top: calc(100% + #{$arrowSize});
       }
 
       &.top {
-        bottom: 100%;
+        bottom: calc(100% + #{$arrowSize});
       }
 
       &.center {
@@ -69,20 +138,31 @@
       &.left {
         right: calc(50% - #{$arrowPosition});
       }
+
+      &.active {
+        display: block;
+      }
     }
   }
 </style>
 
-<div class="tooltip">
-  <slot />
+<div
+  bind:this={wrapperNode}
+  class="tooltip"
+  on:mouseover={handleOnMouseOver}
+  on:mouseout={handleOnMouseLeave}>
   {#if content}
     <div
+      class:active
       class={`tooltip-arrow ${verticalPosition}`}
       style="--background-color: {backgroundColor};" />
     <div
+      bind:this={tooltipContentNode}
+      class:active
       class={`tooltip-content ${verticalPosition} ${horizontalPosition}`}
       style="--background-color: {backgroundColor}; color: {textColor};">
       {content}
     </div>
+    <slot />
   {/if}
 </div>
