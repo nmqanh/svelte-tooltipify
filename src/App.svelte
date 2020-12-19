d51ab3aec7ef5ae0e17884c2a9ac12ca99d5cba7
Anh Nguyen <nmqanh@gmail.com>
2020-12-19 12:08:01 +1000

    Add SASS to project

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
   export let name
 </script>
 
-<style>
+<style type="text/scss">
+  $color: #ff3e00;
+
   main {
     text-align: center;
     padding: 1em;
@@ -11,7 +13,7 @@
   }
 
   h1 {
-    color: #ff3e00;
+    color: $color;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
